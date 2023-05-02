00fd50132af5cdaf1a1ea27647fa5df751bdbbbe
Ariana <akhitrova@ispras.ru>
2023-05-02 13:09:45 +0300

    Add event pages

--- src/main/java/com/Webprac/controllers/EventsController.java
+++ src/main/java/com/Webprac/controllers/EventsController.java
@@ -3,12 +3,11 @@ package com.Webprac.controllers;
 import com.Webprac.DAO.EventDAO;
 import com.Webprac.DAO.EventDAOInterface;
 import com.Webprac.tables.SportEvent;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -20,14 +19,6 @@ public class EventsController {
     @Autowired
     private final EventDAO eventDAO = new EventDAO();
 
-//    @GetMapping("/events")
-//    public String listEvent(Model model) {
-//        Collection<SportEvent> events = eventDAO.getAll();
-//
-//        model.addAttribute("events", events);
-//        model.addAttribute("eventDAO", eventDAO);
-//        return "events";
-//    }
     @GetMapping("/event")
     public String eventPage(@RequestParam(name="eventID") Long eventID, Model model) {
         SportEvent event = eventDAO.getById(eventID);
@@ -57,4 +48,34 @@ public class EventsController {
         return "events";
     }
 
+    @RequestMapping(value="/add_new_event", method = RequestMethod.POST)
+    public String addEvent(@RequestParam(name="title", required = false) String title,
+                           @RequestParam(name="sport", required = false) String sport,
+                           @RequestParam(name="tournament", required = false) String tournament,
+                           @RequestParam(name="description", required = false) String description,
+                           @RequestParam(name="venue", required = false) String venue,
+                           @RequestParam(name="date", required = false) LocalDate date,
+                           @RequestParam(name="seats", required = false) String seats,
+                           @RequestParam(name="results", required = false) String results,
+                           Model model) {
+        SportEvent newEvent = new SportEvent(title, sport, tournament, description,venue, date, null, null);
+        eventDAO.save(newEvent);
+        model.addAttribute("events", eventDAO.getAll());
+        model.addAttribute("eventDAO", eventDAO);
+        return "redirect:/event?eventID=" + newEvent.getId().toString();
+    }
+
+    @RequestMapping(value="/delete_event", method = RequestMethod.POST)
+    public String addEvent(@RequestParam(name="eventID", required = true) Long eventID,
+                           Model model) {
+        eventDAO.deleteById(eventID);
+        model.addAttribute("events", eventDAO.getAll());
+        model.addAttribute("eventDAO", eventDAO);
+        return "redirect:/events";
+    }
+
+    @RequestMapping(value = "addevent")
+    public String addevent() {
+        return "addevent";
+    }
 }
